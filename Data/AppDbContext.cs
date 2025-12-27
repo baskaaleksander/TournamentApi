6017ed4bf6abf0ca5a6c012b0ef47509f90b537d
Aleksander Baska <baskaaleksander03@gmail.com>
2025-12-27 16:59:54 +0100

    Added Entity Framework Core and InMemory packages to the project dependencies.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using TournamentApi.Models;
+
+namespace TournamentApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tournament> Tournaments { get; set; }
+    public DbSet<Bracket> Brackets { get; set; }
+    public DbSet<Match> Matches { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Player1)
+            .WithMany(u => u.MatchesAsPlayer1)
+            .HasForeignKey(m => m.Player1Id)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Player2)
+            .WithMany(u => u.MatchesAsPlayer2)
+            .HasForeignKey(m => m.Player2Id)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Winner)
+            .WithMany(u => u.MatchesAsWinner)
+            .HasForeignKey(m => m.WinnerId)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired(false);
+
+        modelBuilder.Entity<Match>()
+            .HasOne(m => m.Bracket)
+            .WithMany(b => b.Matches)
+            .HasForeignKey(m => m.BracketId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        modelBuilder.Entity<Bracket>()
+            .HasOne(b => b.Tournament)
+            .WithMany(t => t.Brackets)
+            .HasForeignKey(b => b.TournamentId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+    }
+}
+
